7dc6e63d85496013b0f5baeebb731e5c9c743c99
semo <semox78@gmail.com>
2018-04-10 12:56:32 +0200

    zookeeper can now be started from Main. ReadConfigsTest implemented.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2018.  semo
+ */
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.zookeeper.server.ServerConfig;
@@ -7,6 +11,7 @@ import org.apache.zookeeper.server.quorum.QuorumPeerConfig;
 import java.io.IOException;
 import java.util.Map;
 import java.util.Properties;
+
 
 public class Main {
 
@@ -19,41 +24,35 @@ public class Main {
 
         ReadConfigs rc = new ReadConfigs();
         Map<String, String> stuff = rc.getPropertiesAsMap("/opt/kafka_2.11-1.0.1/config/zookeeper.properties");
+//
+//        for (String key: stuff.keySet()) {
+//            System.out.println(key + " " + stuff.get(key));
+//        }
+//
+        Properties props = new Properties();
+//        props.putAll(stuff);
+//        System.out.println(props.stringPropertyNames());
 
-        for (String key: stuff.keySet()) {
-            System.out.println(key + " " + stuff.get(key));
+        props.put("dataDir", stuff.get("dataDir"));
+        props.put("clientPort", "5999");
+
+        QuorumPeerConfig quorumConfiguration = new QuorumPeerConfig();
+        try {
+            quorumConfiguration.parseProperties(props);
+        } catch(Exception e) {
+            throw new RuntimeException(e);
         }
 
-        Properties props = new Properties();
-        props.putAll(stuff);
-        System.out.println(props.stringPropertyNames());
+        ZooKeeperServerMain zooKeeperServer = new ZooKeeperServerMain();
+        final ServerConfig configuration = new ServerConfig();
+        configuration.readFrom(quorumConfiguration);
 
-//        props.put("bootstrap.servers", "localhost:9092");
-//        props.put("enable.auto.commit", "true");
-//        props.put("auto.commit.interval.ms", "1000");
-//        props.put("session.timeout.ms", "30000");
-//        props.put("key.deserializer",
-//                "org.apache.kafka.common.serializa-tion.StringDeserializer");
-//        props.put("value.deserializer",
-//                "org.apache.kafka.common.serializa-tion.StringDeserializer");
-//
-//        QuorumPeerConfig quorumConfiguration = new QuorumPeerConfig();
-//        try {
-//            quorumConfiguration.parseProperties(props);
-//        } catch(Exception e) {
-//            throw new RuntimeException(e);
-//        }
-
-//        ZooKeeperServerMain zooKeeperServer = new ZooKeeperServerMain();
-//        final ServerConfig configuration = new ServerConfig();
-//        configuration.readFrom(quorumConfiguration);
-//
-//        new Thread(() -> {
-//            try {
-//                zooKeeperServer.runFromConfig(configuration);
-//            } catch (IOException e) {
-//                log.error("ZooKeeper Failed", e);
-//            }
-//        }).start();
+        new Thread(() -> {
+            try {
+                zooKeeperServer.runFromConfig(configuration);
+            } catch (IOException e) {
+                log.error("ZooKeeper Failed", e);
+            }
+        }).start();
     }
 }
---
+++ src/test/java/ReadConfigsTest.java
@@ -0,0 +1,61 @@
+import org.junit.Rule;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.migrationsupport.rules.EnableRuleMigrationSupport;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DisplayName("Testing read configuration properties")
+@EnableRuleMigrationSupport
+public class ReadConfigsTest {
+
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+    ;
+    private ReadConfigs rc = new ReadConfigs();
+
+    @DisplayName("Should load a property file and return a map")
+    @Test
+    void loadPropertyFileIntoMap() {
+
+        Properties prop = new Properties();
+        String propFileLocation = null;
+        FileOutputStream output = null;
+
+        try {
+            propFileLocation = folder.getRoot() + "/config.properties";
+            output = new FileOutputStream(propFileLocation);
+
+            prop.setProperty("prop1", "val1");
+            prop.setProperty("prop2", "val2");
+            prop.store(output, null);
+
+        } catch (IOException io) {
+            io.printStackTrace();
+        } finally {
+            if (output != null) {
+                try {
+                    output.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
+
+        Map<String, String> expectedMap = new HashMap<>();
+        expectedMap.put("prop2", "val2");
+        expectedMap.put("prop1", "val1");
+
+        Map<String, String> actualMap = rc.getPropertiesAsMap(propFileLocation);
+        assertEquals(actualMap, expectedMap);
+    }
+
+}
