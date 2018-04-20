6cf21be93fbfedf8783d239c5e93f18c87efa758
semo <semox78@gmail.com>
2018-04-20 14:50:20 +0200

    * preparing for implementing emission to webservice.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -19,34 +19,8 @@ public class Main {
 
         log.trace("Entering application.");
 
-//        NumberPlate np = new NumberPlate();
-//        System.out.println(np.buildPlateImage("."));
-
-        ReadConfigs rc = new ReadConfigs();
-        Map<String, String> stuff = rc.getPropertiesAsMap("/opt/kafka_2.11-1.0.1/config/zookeeper.properties");
-
-        Properties props = new Properties();
-        props.put("dataDir", stuff.get("dataDir"));
-        props.put("clientPort", stuff.get("clientPort"));
-
-        QuorumPeerConfig quorumConfiguration = new QuorumPeerConfig();
-        try {
-            quorumConfiguration.parseProperties(props);
-        } catch(Exception e) {
-            throw new RuntimeException(e);
-        }
-
-        ZooKeeperServerMain zooKeeperServer = new ZooKeeperServerMain();
-        final ServerConfig configuration = new ServerConfig();
-        configuration.readFrom(quorumConfiguration);
-
-        new Thread(() -> {
-            try {
-                zooKeeperServer.runFromConfig(configuration);
-            } catch (IOException e) {
-                log.error("ZooKeeper Failed", e);
-            }
-        }).start();
+        NumberPlate np = new NumberPlate();
+        System.out.println(np.buildPlateImage("."));
 
 
     }
