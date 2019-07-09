a95249bcb06403915ffe7be1d12cc07e686e4c29
semo <semox78@gmail.com>
2019-07-09 06:41:01 +0200

    * reads configs the POJO way from application.properties in RestfulClient.java

--- src/main/java/services/RestfulClient.java
+++ src/main/java/services/RestfulClient.java
@@ -1,27 +1,42 @@
 package services;
 
+import main.Main;
 import models.CamImage;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.*;
-import org.springframework.stereotype.Component;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+@RestController
 public class RestfulClient {
 
     private RestTemplate restTemplate = new RestTemplate();
     private HttpHeaders headers = new HttpHeaders();
 
-    @Value("${kafkaeskadapter.uri}")
-//    private String URI;
-//    private String URI = "http://180.0.0.7:8080/data";
-    private String URI = "http://localhost:9001/postoffice";
+    private String URI;
 
-    public RestfulClient() {}
+    public RestfulClient() throws FileNotFoundException {
+        try (InputStream input = Main.class.getClassLoader().getResourceAsStream("application.properties")) {
+            Properties prop = new Properties();
+            if (input == null) {
+                System.out.println("Sorry, unable to find config.properties");
+                return;
+            }
+            //load a properties file from class path, inside static method
+            prop.load(input);
+            //get the property value and print it out
+            System.out.println(prop.getProperty("kafkaeskadapter.uri"));
+            this.URI = prop.getProperty("kafkaeskadapter.uri");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
 
     public HttpStatus postNumberPlate(CamImage camImage) {
 
@@ -50,7 +65,5 @@ public class RestfulClient {
 
         return result.getStatusCode();
     }
-
-
 }
 
